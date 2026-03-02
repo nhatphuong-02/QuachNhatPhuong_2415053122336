6130bf06c6020c91cb975e54fffaa96e5ca03d74
nhatphuong-02 <phuongquachnhat@gmail.com>
2026-03-02 14:46:42 +0700

    Tuan5

---
+++ Tuan5/Lop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuan5
+{
+    public class Lop<T>
+    {
+        public void TraVeKieu()
+        {
+            Console.WriteLine(typeof(T));
+        }
+    }
+}
---
+++ Tuan5/Program.cs
@@ -0,0 +1,4 @@
+﻿using Tuan5;
+
+Lop<double> lop1 = new Lop<double>();
+lop1.TraVeKieu();
