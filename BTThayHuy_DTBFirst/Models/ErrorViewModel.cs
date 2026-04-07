7b3949d2871f5b02ab6bf4e879737a515bba11f6
nhatphuong-02 <phuongquachnhat@gmail.com>
2026-04-07 20:26:04 +0700

    Xóa thư mục BTThayHuy_DTBFirst

--- BTThayHuy_DTBFirst/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BTThayHuy_DTBFirst.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
